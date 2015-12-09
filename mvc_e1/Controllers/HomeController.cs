4f4efdb05c656fff5b92cea42bb99ff2d45526c4
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-12-09 20:07:22 +0200

    e1

---
+++ mvc_e1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_e1.Models;
+
+namespace mvc_e1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good morning" : "good afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ mvc_e1/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc_e1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please, your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please, your mail")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please, your Phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please, select an option")]
+        public bool? WillAttend { get; set; }
+    }
+}

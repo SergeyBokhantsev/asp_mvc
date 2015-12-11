9fe64e2ca40801ac9feba189db3853becabc950b
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-12-11 23:39:06 +0200

    e3

---
+++ mvc_e3/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_e3.Controllers;
+using mvc_e3.Models;
+using Ninject;
+
+namespace mvc_e3.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>().WhenInjectedInto<HomeController>().WithPropertyValue("DiscountSize", 50M);
+        }
+    }
+}
---
+++ mvc_e3/Models/LinqValueCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_e3.Models
+{
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+
+    public class LinqValueCalculator : IValueCalculator
+    {
+        public decimal DiscountSize { get; set; }
+
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price) - DiscountSize;
+        }
+    }
+}
